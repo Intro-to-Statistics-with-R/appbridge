f5e0cb90bf4d665382b84ffabb424f492ce47e1a
Kelly N. Bodwin <kelly@bodwin.us>
2025-01-13 00:08:58 -0800

    small test code

--- 01-summarize_data/app.R
+++ 01-summarize_data/app.R
@@ -1,7 +1,49 @@
 library(tidyverse)
 library(shiny)
 
-summarize_data_app <- function() {
+mod_glimpse_data_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    radioButtons(NS(id, "glimpse"),
+                 "What information about the dataset would you like?",
+                 choices = c(
+                   "Just show me the whole thing!" = "raw",
+                   "Show me the first five rows only." = "head",
+                   "Show me the last five rows only." = "tail",
+                   "Show me a list of the variables and their types." = "glimpse",
+                   "Calculate some summaries for each variable." = "summary"
+
+                 )),
+    verbatimTextOutput(NS(id, "glimpse_code")),
+    textOutput(NS(id, "glimpse_result"))
+  )
+}
+
+#' glimpse_data Server Functions
+#'
+#' @noRd
+#' @export
+mod_glimpse_data_server <- function(id, titanic){
+  moduleServer(id, function(input, output, session){
+    ns <- session$ns
+    output$glimpse_code <- renderPrint(switch(
+      input$glimpse,
+      raw = "titanic",
+      head = "head(titanic, 5)",
+      tail = "tail(titanic, 5)",
+      glimpse = "glimpse(titanic)",
+      summary = "summary(titanic)"
+    ))
+
+    output$glimpse_result <- renderPrint({
+      glimpse(titanic)
+      #eval(parse(text = input$glimpse_code))
+    })
+  })
+}
+
+
+#summarize_data_app <- function() {
 
   ui <- fluidPage(
     mod_glimpse_data_ui("dat1")
@@ -24,4 +66,4 @@ summarize_data_app <- function() {
     mod_glimpse_data_server("dat1", titanic)
   }
   shinyApp(ui, server)
-}
+#}
